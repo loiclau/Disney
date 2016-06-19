1b66b4dba42744894a0fe2a532b6c88171d70b52
loiclau <loic.lau@gmail.com>
2016-06-19 20:33:57 +0200

    :fire:(Figaro) step1 intro

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php
+
+class Personnage // Présence du mot-clé class suivi du nom de la classe.
+
+{
+    private $_force;        // La force du personnage
+    private $_localisation; // Sa localisation
+    private $_experience;   // Son expérience
+    private $_degats;       // Ses dégâts
+
+
+    public function deplacer() // Une méthode qui déplacera le personnage (modifiera sa localisation).
+    {
+
+    }
+
+    public function frapper() // Une méthode qui frappera un personnage (suivant la force qu'il a).
+    {
+
+    }
+
+    public function gagnerExperience() // Une méthode augmentant l'attribut $experience du personnage.
+    {
+
+    }
+
+}
