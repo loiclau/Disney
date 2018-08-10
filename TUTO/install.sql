1f950fe9edb00f035bba9b21775dafe34c6d6c6a
Loic Laurent <loic.lau@gmail.com>
2018-08-10 17:08:41 +0200

    :fire:(Figaro) move tuto

--- TUTO/install.sql
+++ TUTO/install.sql
@@ -21,6 +21,9 @@ CREATE TABLE IF NOT EXISTS `personnagestp1` (
   `id` smallint(5) unsigned NOT NULL AUTO_INCREMENT,
   `nom` varchar(50) COLLATE utf8_general_ci NOT NULL,
   `degats` tinyint(3) unsigned NOT NULL DEFAULT '0',
+  `lvl` tinyint(3) unsigned NOT NULL DEFAULT '0',
+  `exp` tinyint(3) unsigned NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`),
   UNIQUE KEY `nom` (`nom`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
