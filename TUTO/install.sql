a46247ae990029f1fde06ce8e16d5de49cd9ad16
Loic Laurent <loic.lau@gmail.com>
2018-08-16 17:49:27 +0200

    :fire:(Figaro) init Tp2

--- TUTO/install.sql
+++ TUTO/install.sql
@@ -2,7 +2,6 @@ CREATE USER 'iamroot'@'localhost' IDENTIFIED BY 'iamroot';
 GRANT ALL PRIVILEGES ON * . * TO 'iamroot'@'localhost';
 FLUSH PRIVILEGES;
 
-
 CREATE DATABASE ocpoo;
 
 CREATE TABLE IF NOT EXISTS `personnages` (
@@ -16,7 +15,6 @@ CREATE TABLE IF NOT EXISTS `personnages` (
   UNIQUE KEY `nom` (`nom`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
-
 CREATE TABLE IF NOT EXISTS `personnagestp1` (
   `id` smallint(5) unsigned NOT NULL AUTO_INCREMENT,
   `nom` varchar(50) COLLATE utf8_general_ci NOT NULL,
@@ -28,3 +26,16 @@ CREATE TABLE IF NOT EXISTS `personnagestp1` (
   UNIQUE KEY `nom` (`nom`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
+CREATE TABLE IF NOT EXISTS `personnagestp2` (
+  `id` smallint(5) unsigned NOT NULL AUTO_INCREMENT,
+  `nom` varchar(50) COLLATE utf8_general_ci NOT NULL,
+  `degats` tinyint(3) unsigned NOT NULL DEFAULT '0',
+  `lvl` tinyint(3) unsigned NOT NULL DEFAULT '0',
+  `exp` tinyint(3) unsigned NOT NULL DEFAULT '0',
+  `force` tinyint(3) unsigned NOT NULL DEFAULT '0',
+  `timeEndormi` int(10) unsigned NOT NULL DEFAULT '0',
+  `type` enum('magicien','guerrier') COLLATE utf8_general_ci NOT NULL,
+  `atout` tinyint(3) unsigned NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `nom` (`nom`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
