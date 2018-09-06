e1ee71c0c15119a28506b9516c38c61365a810d2
Loic Laurent <loic.lau@gmail.com>
2018-09-06 13:40:51 +0200

    :snake:(Kaa) first example exception

--- ex1/bissextile.py
+++ ex1/bissextile.py
@@ -2,7 +2,13 @@
 # -*- coding: utf-8 -*-
 
 annee = input("saisissez une année : ")
-annee = int(annee)
+try:
+    annee = int(annee)
+    assert annee > 0
+except ValueError:
+    print("Vous n'avez pas saisi un nombre.")
+except AssertionError:
+    print("L'année saisie est inférieure ou égale à 0.")
 
 if annee % 400 == 0 or (annee % 4 ==0 and annee % 100 !=0):
     print("L'année saisie est bissextile.")
