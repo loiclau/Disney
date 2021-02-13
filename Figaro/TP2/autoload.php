e7ed77bf90e1a154f24cb5a55d2005d93204eec5
loiclau <llaurent@centreon.com>
2021-02-13 22:37:31 +0100

    Delete Figaro directory

--- Figaro/TP2/autoload.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-function chargerClasse($classe)
-{
-require './entity/' . $classe . '.class.php'; // On inclut la classe correspondante au paramètre passé.
-}
-
-spl_autoload_register('chargerClasse');
